eebb6e5191d130520b9ca7ed4c013308de4cf2eb
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-02 22:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const timeEle = document.getElementById("timer");
+
+
+//let timme = `
+//${tim.getMonth()}/${tim.getDate()}/${tim.getFullYear()}, ${tim.getHours()}:${tim.getMinutes()}:${tim.getSeconds()}`;
+
+
+setInterval(()=>{
+	const tim = new Date();
+	const ti = tim.toLocaleString("en-us",{hour12:true});
+	
+	timeEle.innerHTML = ti;
+},1000);
